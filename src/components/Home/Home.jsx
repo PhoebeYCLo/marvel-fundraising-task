751f55358115bb3113be6907e5dac1cb480fc3a3
Phoebe Lo <phoebeyclo@gmail.com>
2021-04-10 15:45:39 -0700

    Add Hero component to Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,1 +1,14 @@
 import React, { Component } from 'react';
+import Hero from '../Hero/Hero';
+
+class Home extends Component{
+    render() {
+        return (
+            <>
+                <Hero />
+            </>
+        )
+    }
+}
+
+export default Home;
