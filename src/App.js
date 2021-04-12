38e2daea3da0c4899ca43638b7b2b4ab4e43eac6
Phoebe Lo <phoebeyclo@gmail.com>
2021-04-12 02:57:00 -0700

    Create route for donate page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import DonatePage from './components/DonatePage/DonatePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/donate" component={DonatePage} />
+        </Switch>
     </div>
   );
 }
