9d9f3171a731f4bf55cf9c64be28a14a76face63
Phoebe Lo <phoebeyclo@gmail.com>
2021-04-12 02:30:45 -0700

    Add donate hero page to home page

---
+++ src/components/DonateHero/DonateHero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './donatehero.scss';
+
+function donateHero() {
+    return(
+        <div className="donatehero">
+            <div className="donatehero__content container">
+                <div className="donatehero__header">You have the power to advocate for justice around the world. Make your gift to Neighborhood Defense today.</div>
+                <button className="btn__secondary donatehero__button">Donate now</button>
+            </div>
+        </div>
+    )
+}
+
+export default donateHero;
