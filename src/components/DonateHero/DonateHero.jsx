254eb89fb5e92a8d1ba69049571804f89106e705
Phoebe Lo <phoebeyclo@gmail.com>
2021-04-12 12:59:45 -0700

    Donate button add route to Donate page

--- src/components/DonateHero/DonateHero.jsx
+++ src/components/DonateHero/DonateHero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './donatehero.scss';
 
 function DonateHero() {
@@ -6,7 +7,9 @@ function DonateHero() {
         <div className="donatehero">
             <div className="donatehero__content container">
                 <div className="donatehero__header">You have the power to advocate for justice around the world. Make your gift to Neighborhood Defense today.</div>
-                <button className="btn__secondary donatehero__button">Donate now</button>
+                <Link to={`/donate`}>
+                    <button className="btn__secondary donatehero__button">Donate now</button>
+                </Link>
             </div>
         </div>
     )
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './hero.scss';
 
 function Hero() {
@@ -8,7 +9,9 @@ function Hero() {
                 <div className="hero__header">
                     <h1 className="hero__header-text">Support your friendly<br/> 
                     neighborhood superhero</h1>
+                <Link to={`/donate`}>
                     <button className="btn__primary">Donate now</button>
+                </Link>
                 </div>
             </div>
         </div>
