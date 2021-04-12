77a94a94006dd28d6344563271f1598a686b3bf0
Phoebe Lo <phoebeyclo@gmail.com>
2021-04-12 13:03:24 -0700

    Page change scroll to top

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import DonatePage from './components/DonatePage/DonatePage';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
       <Header />
+        <ScrollToTop />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/donate" component={DonatePage} />
