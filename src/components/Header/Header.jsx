e3e3fa6e4fab0ae26a6a4a33eed95bf03bdae138
Phoebe Lo <phoebeyclo@gmail.com>
2021-04-12 03:03:38 -0700

    Add route to header nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/logo.png';
 import './header.scss';
 
@@ -8,12 +9,16 @@ function Header() {
             <div className="header__left">
                 <img src={Logo} alt="Logo" />
                 <div className="header__logo">
-                    <h1 className="header__logo-header">MARVEL</h1>
-                    <h4 className="header__logo-subheader">Neighborhood Defense</h4>
+                    <Link to={`/`}>
+                        <h1 className="header__logo-header">MARVEL</h1>
+                        <h4 className="header__logo-subheader">Neighborhood Defense</h4>
+                    </Link>
                 </div>
             </div>
             <div className="header__right">
-                <button className="btn__primary">Donate now</button>
+                <Link to={`/donate`}>
+                    <button className="btn__primary">Donate now</button>
+                </Link>
             </div>
         </div>
     )
