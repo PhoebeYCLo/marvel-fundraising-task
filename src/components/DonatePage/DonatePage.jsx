7427ece09805d3e81083cef1d2c2077bdfe86b07
Phoebe Lo <phoebeyclo@gmail.com>
2021-04-12 02:57:40 -0700

    Create initial donate page

---
+++ src/components/DonatePage/DonatePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './donatepage.scss';
+
+function DonatePage() {
+    return(
+        <div>
+            DonatePage
+        </div>
+    )
+}
+
+export default DonatePage;
